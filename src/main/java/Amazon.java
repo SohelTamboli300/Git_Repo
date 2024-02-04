12303deb355e7c75cabebfd7d6bf5dce53113468
sohel <soheltamboli300@gmail.com>
2024-02-04 12:43:25 +0530

    commited

--- src/main/java/Amazon.java
+++ src/main/java/Amazon.java
@@ -9,7 +9,7 @@ public class Amazon {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/gp/homepage.html?_encoding=UTF8&ref_=navm_accountmenu_re_signout&path=%2Fgp%2Fhomepage.html%3F_encoding%3DUTF8%26ref_%3Dnavm_accountmenu_re_signout&useRedirectOnSuccess=1&signIn=1&action=sign-out&ref_=navm_accountmenu_signout&");
-		
+		//this is comment
 	}
 
 }
